e97c8f95ea6fccb6b01b37c33b8bb0cea0f4e4a1
FengCheng <hellcan@live.cn>
2018-04-26 12:17:02 -0500

    project and task tab screen

--- app/src/main/java/b12app/vyom/com/flowit/networkutils/ApiService.java
+++ app/src/main/java/b12app/vyom/com/flowit/networkutils/ApiService.java
@@ -11,13 +11,17 @@ import retrofit2.http.Query;
 public interface ApiService {
 
     @POST("pms_reg.php?")
-    Call<User>postUser(@Query("first_name") String first_name, @Query("last_name") String last_name,@Query("email") String email
-    , @Query("mobile") String mobile, @Query("password") String password, @Query("company_size") String company_size, @Query("your_role") String your_role);
+    Call<User> postUser(@Query("first_name") String first_name,
+                        @Query("last_name") String last_name,
+                        @Query("email") String email,
+                        @Query("mobile") String mobile,
+                        @Query("password") String password,
+                        @Query("company_size") String company_size,
+                        @Query("your_role") String your_role);
 
     @GET("shop_login.php")
-    Call<List<User>> getUser(@Query("mobile") String mobileNumber, @Query("password") String password);
-
-
+    Call<List<User>> getUser(@Query("mobile") String mobileNumber,
+                             @Query("password") String password);
 
 
 }
