0de8766ff169cf93709bfd33e1ad44c9077dfed8
Vyom Patel <vyomhpatel@gmail.com>
2018-05-01 10:07:22 -0500

    Reverted Changes and merge with feng cheng MVP

--- app/src/main/java/b12app/vyom/com/flowit/projectcreate/ProjectCreateActivity.java
+++ app/src/main/java/b12app/vyom/com/flowit/projectcreate/ProjectCreateActivity.java
@@ -15,7 +15,6 @@ import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
 
